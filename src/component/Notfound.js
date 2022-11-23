b72284b6cb01a1022f707788655bba8f2df62cdd
GOKULAKRISHNAN . J <110898672+gokulakrishnan-j@users.noreply.github.com>
2022-11-23 14:23:11 +0530

    Update Notfound.js

--- src/component/Notfound.js
+++ src/component/Notfound.js
@@ -3,7 +3,7 @@ import '../App.css';
 function Notfoun() {
   return (
     <div>
-      <img className="notfoun" src='https://cdn.dribbble.com/users/1138875/screenshots/4669703/404_animation.gif' alt='Not found' />
+      <img className="notfound" src='https://cdn.dribbble.com/users/1138875/screenshots/4669703/404_animation.gif' alt='Not found' />
     </div>
   )
 }
